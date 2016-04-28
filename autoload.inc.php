e37ed4a9eb60efd6a8a76e67d65bc33783606242
Layer <gai00layer@gmail.com>
2016-04-28 06:28:47 +0000

    add autoload.inc.php for easily to use.

---
+++ autoload.inc.php
@@ -0,0 +1,8 @@
+<?php
+    // for autoloading easily.
+    spl_autoload_register(function($class) {
+        $path = __DIR__ . "/libs/" . str_replace('\\', '/', $class) . '.php';
+        if(file_exists($path)) {
+            include_once($path);
+        }
+    });
